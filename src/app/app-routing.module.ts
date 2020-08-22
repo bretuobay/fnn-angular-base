8fd669e6da338066415a86c7de3a88fa2b3b584b
fyeboah <festus.yeboah@konux.de>
2020-08-22 17:30:48 +0200

    Add component files and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CultureComponent } from './components/culture/culture.component';
+import { EntertainmentComponent } from './components/entertainment/entertainment.component';
+import { HomeComponent } from './components/home/home.component';
+import { TechnologyComponent } from './components/technology/technology.component';
+import { ScienceComponent } from './components/science/science.component';
+import { BusinessComponent } from './components/business/business.component';
+import { VideosComponent } from './components/videos/videos.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'culture', component: CultureComponent },
+  { path: 'entertainment', component: EntertainmentComponent },
+  { path: 'technology', component: TechnologyComponent },
+  { path: 'science', component: ScienceComponent },
+  { path: 'business', component: BusinessComponent },
+  { path: 'videos', component: VideosComponent },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CultureComponent } from './components/culture/culture.component';
+import { EntertainmentComponent } from './components/entertainment/entertainment.component';
+import { HomeComponent } from './components/home/home.component';
+import { TechnologyComponent } from './components/technology/technology.component';
+import { ScienceComponent } from './components/science/science.component';
+import { BusinessComponent } from './components/business/business.component';
+import { VideosComponent } from './components/videos/videos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CultureComponent,
+    EntertainmentComponent,
+    HomeComponent,
+    TechnologyComponent,
+    ScienceComponent,
+    BusinessComponent,
+    VideosComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
