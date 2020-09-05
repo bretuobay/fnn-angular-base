bc71a7d794ecc2f81ab38aa5e1edabd95d099497
fyeboah <festus.yeboah@konux.de>
2020-09-05 21:12:30 +0200

    Final commit before introducing ngrx

--- src/app/components/videos/videos.component.ts
+++ src/app/components/videos/videos.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-videos',
   templateUrl: './videos.component.html',
-  styleUrls: ['./videos.component.css']
+  styleUrls: ['./videos.component.css'],
 })
 export class VideosComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
